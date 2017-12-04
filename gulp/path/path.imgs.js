5b9ad1f06fba80910a7818ae53c3b5da0ce20733
Nikita Laitinen <nikitalaitinen@ya.ru>
2017-12-04 19:55:19 +0300

    update img.opt task

--- gulp/path/path.imgs.js
+++ gulp/path/path.imgs.js
@@ -1,5 +1,5 @@
 module.exports = {
-	content: './app/imgs/content/*.*',
+	content: './app/imgs/content/*.+(png|jpg)',
 	decorate_png: './app/imgs/decorate/png/*.*',
 	decorate_svg: './app/imgs/decorate/svg/*.svg',
 };
